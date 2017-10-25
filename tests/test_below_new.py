27206b8018d266ad4a8bf732a34bdb8a17b97993
b.pustovyi <b.pustovyi@e-tender.biz>
2017-10-25 18:44:37 +0300

    chomedriver may be fixed

--- tests/test_below_new.py
+++ tests/test_below_new.py
@@ -13,7 +13,6 @@ from core.tools import *
 from core.conditions import *
 from selenium.webdriver.support.ui import Select
 from core.et_data import users
-from chromedriver import CHROMEDRV_PATH
 
 path = "http://40.69.95.23/#/"
 path = "http://192.168.103.42/#/"
