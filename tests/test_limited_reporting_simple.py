6ac003a4bc29fb8a40a707925972209f185b5ed7
s.dmitriev <s.dmitriev@e-tender.biz>
2018-01-05 14:33:15 +0200

    add limited_reporting_simple suite

---
+++ tests/test_limited_reporting_simple.py
@@ -0,0 +1,16 @@
+from tests.base_test import  *
+
+
+class TestOwnerSuite(BaseOwnerTest):
+    tender_type = 'reporting'
+
+
+# class TestViewerSuite(BaseViewerTest):
+#     pass
+
+# class TestProviderTest(BaseProviderTest):
+#    pass
+
+
+if '__name_' == '__main__':
+    pass
